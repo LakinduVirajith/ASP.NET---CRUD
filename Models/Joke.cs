b827873228531a2f62ee7782f6ababac46ae4897
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-26 17:38:27 +0530

    jokes crud implementation

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP.NET___CRUD.Models
+{
+    public class Joke
+    {
+        // prop
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        // ctor
+        public Joke()
+        {
+            
+        }
+    }
+}
